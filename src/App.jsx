68a99752b79c0d2eaca51aebe339d90c78930935
Plesh <pleshkunov99@example.com>
2026-03-02 04:27:40 +0100

    feat: добавлена змея

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/layout/Navbar";
 import { Hero } from "@/sections/Hero";
 import { Hero1 } from "@/sections/Hero1";
+import { About } from "@/sections/About";
 import { Pricing } from "@/sections/Pricing";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="min-h-screen overflow-x-hidden">
       <Hero1 />
       <Pricing />
+      <About />
       <main></main>
     </div>
   );
